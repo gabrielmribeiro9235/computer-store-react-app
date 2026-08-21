c97a1fc0ed0083c6e0abd9c6ed5848838d18b925
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-08-21 12:14:36 -0300

    feat: adiciona a função para gerenciar a mudança de estado dos atributos do DropDown

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -1,10 +1,14 @@
 import "./DropDown.css";
 
 const DropDown = (props) => {
+    const handleSelection = (event) => {
+        props.setValue(event.target.value);
+    }
+
     return (
         <div className="drop-down">
             <label>{props.label}</label>
-            <select>
+            <select value={props.value} onChange={(event) => handleSelection(event)}>
                 {props.items.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
