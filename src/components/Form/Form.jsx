128d7cb5e5962bd59d4844a06760643f2f3b0880
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-08-21 10:49:10 -0300

    feat: cria o componente DropDown

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,29 @@
 import "./Form.css";
 import InputText from "../InputText/InputText";
+import DropDown from "../DropDown/DropDown";
 
 const Form = (props) => {
+    const sections = [
+        "Computadores",
+        "Acessórios",
+        "Impressoras",
+        "Games",
+        "Gadgets"
+    ];
+
+    const brands = [
+        "HP",
+        "Dell",
+        "Positivo",
+        "Asus",
+        "Xing Ling genérico",
+    ];
+
     return (
         <section className="formulario">
             <form>
+                <DropDown label="Seção:" items={sections} />
+                <DropDown label="Marca:" items={brands} />
                 <InputText label="Nome:" placeholder="Digite o nome do produto" />
                 <InputText label="Preço:" placeholder="Digite o preço do produto" />
             </form>
