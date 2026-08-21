0dde1c3235f06bf5c565291cafdc5514481dfe02
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-08-21 12:14:58 -0300

    feat: adiciona a função para gerenciar a mudança de estado dos atributos do InputText

--- src/components/InputText/InputText.jsx
+++ src/components/InputText/InputText.jsx
@@ -1,10 +1,14 @@
 import "./InputText.css";
 
 const InputText = (props) => {
+    const handleOnChange = (event) => {
+        props.setValue(event.target.value);
+    }
+
     return ( 
         <div className="input-text">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder} type="text" />
+            <input placeholder={props.placeholder} value={props.value} type="text" onChange={(event) => handleOnChange(event)} />
         </div>
      );
 }
