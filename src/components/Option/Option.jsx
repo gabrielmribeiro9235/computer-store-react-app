a0ec48a85e838ba5fe5a9caf836d9b61d2f45611
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-08-21 12:15:21 -0300

    feat: adiciona a função para gerenciar a mudança de estado dos atributos do Option

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,11 +1,15 @@
 import "./Option.css";
 
 const Option = (props) => {
+    const handleOption = (event) => {
+        props.setSelectedStatus(event.target.value);
+    }
+
     return (
         <div className="option">
             {props.options.map(option => (
                 <div key={option}>
-                    <input name="option" type="radio" />
+                    <input name="option" type="radio" value={option} checked={props.selectedStatus === option} onChange={(event) => handleOption(event)} />
                     <label>{option}</label>
                 </div>
             ))}
