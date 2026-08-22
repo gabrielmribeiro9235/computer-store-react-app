cd39d26b7f0b2ef348a9c576dd660fcd2da9490d
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-08-22 11:31:13 -0300

    refactor: transforma os elementos do array sections em objetos que contém nome, cor primária e secundária

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,37 @@ import { useState } from "react"
 function App() {
   const [products, setProducts] = useState([]);
   const sections = [
-      "Computadores",
-      "Acessórios",
-      "Impressoras",
-      "Games",
-      "Gadgets"
+    {
+      name: "Computadores",
+      primaryColor: "#57c278",
+      secundaryColor: "#d9f7e9"
+    },
+    {
+      name: "Acessórios",
+      primaryColor: "#82cffa",
+      secundaryColor: "#e8f8ff"
+    },
+    {
+      name: "Impressoras",
+      primaryColor: "#a6d157",
+      secundaryColor: "#f0f8e2"
+    },
+    {
+      name: "Games",
+      primaryColor: "#e06b69",
+      secundaryColor: "#fde7e8"
+    },
+    {
+      name: "Gadgets",
+      primaryColor: "#8b6fd1",
+      secundaryColor: "#f0ecfa"
+    }
   ];
 
   return (
     <div>
       <Header>Dados do produto</Header>
-      <Form products={products} setProducts={setProducts} sections={sections} />
+      <Form products={products} setProducts={setProducts} sections={sections.map(section => section.name)} />
     </div>
   )
 }
